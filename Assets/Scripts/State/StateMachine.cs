6757413fee91fd18edc0e68980cd6d849a22f4b9
EgeISerbes <egeserbes@hotmail.com>
2024-01-27 23:33:59 +0300

    state machine

--- Assets/Scripts/State/StateMachine.cs
+++ Assets/Scripts/State/StateMachine.cs
@@ -34,6 +34,13 @@ namespace KillingJoke.Core
         public SelectorUnityEventWrapper PalmUpRight { get => palmUpRight; set => palmUpRight = value; }
         public SelectorUnityEventWrapper PalmUpLeft { get => palmUpLeft; set => palmUpLeft = value; }
 
+
+        private float _time_to_forgive = 1f;
+        private Coroutine _coroutineForgive;
+        private Coroutine _coroutineKill;
+        private float _time_to_kill = 1f;
+        private bool _canPlay = false;
+
         public void Init(GameManager gameManager)
         {
             _gameManager = gameManager;
@@ -58,7 +65,40 @@ namespace KillingJoke.Core
         {
             _gameManager.CurrentSession.KillJoker(_gameManager.ActiveJoker);
         }
+        public void StartForgive()
+        {
+            _coroutineForgive = StartCoroutine(ForgiveTimer());
+        }
+        public void StartKill()
+        {
+            _coroutineKill = StartCoroutine(KillTimer());
+        }
+        IEnumerator ForgiveTimer()
+        {
+            yield return new WaitForSeconds(_time_to_forgive);
+            Forgive();
+        }
+        IEnumerator KillTimer()
+        {
+            yield return new WaitForSeconds(_time_to_kill);
+            Kill();
 
+        }
+        public void StopForgivingAction()
+        {
+            if (_coroutineForgive != null)
+            {
+                StopCoroutine(_coroutineForgive);
+            }
+        }
+        public void StopKillingAction()
+        {
+            if (_coroutineKill != null)
+            {
+                StopCoroutine(_coroutineKill);
+            }
+
+        }
         public void StartTell()
         {
             _gameManager.StartListenPlayer();
